5ea7e4a1a5e2401f8f835841d583920e36791aa1
NCJ Charlie <me@ncj.wiki>
2022-01-24 11:22:02 +0800

    add dense

--- examples/get_rough_poisson.py
+++ examples/get_rough_poisson.py
@@ -2,11 +2,13 @@ from missing_kit.io import create_colmap_database
 from missing_kit.shell import colmap
 
 
-def get_poisson_mesh(database_name, undistorted_image_path, camera_params, sparse_model_path):
+def get_poisson_mesh(database_name, undistorted_image_path, camera_params, sparse_model_path, dense_model_path):
     create_colmap_database(database_name)
     colmap.feature_extractor(database_name, undistorted_image_path, camera_params)
     colmap.exhaustive_matcher(database_name)
     colmap.reconstruction_mapper(database_name, undistorted_image_path, sparse_model_path)
+    colmap.bundle_adjustment(sparse_model_path + '/0')
+    colmap.image_undistortion(undistorted_image_path, sparse_model_path + '/0', dense_model_path)
 
 
 if __name__ == '__main__':
@@ -14,11 +16,13 @@ if __name__ == '__main__':
     DB_FILE = f'{BASE_PATH}/database.db'
     IMAGE_PATH = f'{BASE_PATH}/30views'
     CAMERA_PARAMS = '2373.046104729776,2375.5106693944517,668.8785376738697,550.609404815664,0.0,0.0,0.0,0.0'
-    SPARSE_MODEL = f'{BASE_PATH}/sparse'
+    SPARSE_MODEL_PATH = f'{BASE_PATH}/sparse'
+    DENSE_MODEL_PATH = f'{BASE_PATH}/dense'
 
     get_poisson_mesh(
         database_name=DB_FILE,
         undistorted_image_path=IMAGE_PATH,
         camera_params=CAMERA_PARAMS,
-        sparse_model_path=SPARSE_MODEL
+        sparse_model_path=SPARSE_MODEL_PATH,
+        dense_model_path=DENSE_MODEL_PATH
     )
--- missing_kit/shell/colmap.py
+++ missing_kit/shell/colmap.py
@@ -62,5 +62,39 @@ def reconstruction_mapper(database_file, image_path, output_path):
         raise RuntimeError('COLMAP Mapper run failed!')
 
 
-def bundle_adjustment():
-    pass
+def bundle_adjustment(model_path):
+    """
+    Wrapper of COLMAP Bundle Adjuster
+    :param model_path:
+    """
+    stdout, stderr, exit_code = sh('colmap bundle_adjuster '
+                                   f'--input_path {model_path} '
+                                   f'--output_path {model_path} '
+                                   '--BundleAdjustment.refine_focal_length 0'
+                                   '--BundleAdjustment.refine_principal_point 0'
+                                   '--BundleAdjustment.refine_extra_params 0'
+                                   '--BundleAdjustment.refine_extrinsics 1')
+
+    if exit_code != 0:
+        print(stdout)
+        print(stderr)
+        raise RuntimeError('COLMAP Bundle Adjuster run failed!')
+
+
+def image_undistortion(image_path, model_path, output_path):
+    """
+    Wrapper of COLMAP Image Undistortor
+    :param image_path:
+    :param model_path:
+    :param output_path:
+    """
+    # TODO: add roi
+    stdout, stderr, exit_code = sh('colmap image_undistorter '
+                                   f'--input_path {model_path} '
+                                   f'--image_path {image_path} '
+                                   f'--output_path {output_path} ')
+
+    if exit_code != 0:
+        print(stdout)
+        print(stderr)
+        raise RuntimeError('COLMAP Image Undistortor run failed!')
